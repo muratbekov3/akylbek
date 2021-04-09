d16f8788fef162cc4b9686cff0bee92272b7d55b
akylbek muratbekov <60873254+muratbekov3@users.noreply.github.com>
2021-04-09 11:55:03 +0600

    Update register.php

--- register.php
+++ register.php
@@ -1,203 +1,121 @@
-<!-- <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div class="div">
-        <form  method="get">
-            <label for="login">Username</label>
-            <input type="text" id="fname" name="fname" ><br><br>
-            <label for="password" >Password</label>
-            <input  id="lname" type="password" name="lname"><br><br>
-            <button onclick="myFunction()">login</button>
-            <a href="https://instagram.com/" id="a"></a>
-
-          </form>
-          
-
-          
-    </div>
-    <h1 id="demo"></h1>
-    <script> 
-      function myFunction(){
-var x = document.getElementById('fname').value
-var b = document.getElementById('lname').value
-var form = document.querySelector('form')
-
-document.getElementById("demo").innerHTML = x;
-
-
-
-if (x == "akylbek" , b == "12345"){
-    form.setAttribute("action", "https://instagram.com/")
-
-}else{
-    alert("permission denied")
-}
-
-}
-    </script>
-</body>
-</html> -->
-
-
 <!DOCTYPE html>
 <html>
-    <head>
-        <tittle></tittle>
-    </head>
-    <style>body {
-    min-height:100%;
-    background-color: #263238;
+<head>
+<title>login</title>
+  <style>
+  @import url('https://fonts.googleapis.com/css?family=Raleway&display=swap');
+* {
+	box-sizing: border-box;
 }
-
-.text-center{
-  text-align: center;
+body {
+	margin: 0;
+	padding: 0;
+	font-family: 'Raleway', sans-serif;
 }
-
-.space-bot{
-  margin-bottom: 35px;
+.box {
+	display: flex;
+	background-color: white;
+	align-items: center;
+	justify-content: center;
+	background-color: #21D4FD;
+	background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
+  	height: 100vh;
 }
-
-.space-top{
-  margin-top: 35px;
+.login-form {
+  	min-width: 250px;
+  	max-width: 400px;
+	border-radius: 24px;
+	padding: 15px;
+	background-color: white;
+	box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
 }
-
-/* Title */
-
-h1 {
-    color: white;
-    font-family: 'Roboto';
-    text-transform: uppercase;
-    font-weight: 900;
-    font-size: 25px;
-    text-align: center;
-}
-
-/* Sign Up */
-
-.container {
-    max-width: 400px;
-    max-height: 100px;
-    margin-top: 10px;
-}
-
-.alert-danger{
+.login-form h1 {
 	text-align: center;
+	font-size: 2.5rem;
+	margin-top: 35px;
 }
-
-.signup-screen {
-	  padding: 20px;
-    padding-bottom: 40px;
-    border-radius: 5px;
-    background-color: #2c3940;
-    box-shadow: 0 1px 6px rgba(0,0,0,.3);
-    color: white;
+.login-form input[type = "text"] {
+	margin-top: 30px;
 }
-
-.btn{
-  border-radius: 2px;
+.login-form input[type = "password"] {
+	margin-top: 10px;
 }
-
-.cancel{
-  background-color: #df405a;
+input {
+  outline: none;
+ }
+.links {
+	margin-top: 10px;
+	display: flex;
+	flex-wrap: wrap;
+	flex-direction: row;
 }
-
-.done{
-  background-color: #5CAB7D;
+.links > a:first-of-type {
+	margin-right: 5px;
 }
-
-.done:hover{
-  background-color: #6dc793;
+.links > a {	
+	background-color: #E0E0E0;
+	border-radius: 24px;
+	font-weight: 400;
+	color: black;
+	line-height: 12px;
+	flex: 1;
+	text-align: center;
+	padding: 10px;
+	text-decoration: none;
+  font-family: 'Raleway';
+	transition: 0.25s;
 }
-
-.done:focus{
-  background-color: #6dc793;
+.links > a:hover {	
+	opacity: 0.6;
 }
-
-.cancel:hover{
-  background-color: #f0435f;
+.login-form input[type = "submit"] {
+	background-color:  #E0E0E0;
+	background-image: linear-gradient(19deg, #21D4FD 0%, #B721FF 100%);
+	width: 100%;
+	color: white;
+  	border: none;
+	margin-top: 35px;
+  	cursor: pointer;
+	padding: 10px;
+  	font-family: 'Raleway', sans-seriff;
+  	font-size: 1.3rem;
+	font-weight: bold;
+	border-radius: 24px;
+	transition: 0.25s;
 }
-
-.cancel:focus{
-  background-color: #f0435f;
+.login-form input[type = "submit"]:hover {
+	opacity: 0.8;
 }
+.login-form input[type = "text"]:focus, .login-form input[type = "password"]:focus {
+ 	border: 2px #21D4FD solid;
+ }
+.login-form input[type = "text"], .login-form input[type = "password"] {
+	width: 100%;
+	border: none;
+	border-radius: 24px;
+  	font-size: 1rem;
+  	font-family: 'Raleway', sans-seriff;
+	background-color: gainsboro;
+	padding: 15px;
+} 
+  </style>
+</head>
+<body>
 
-.input-group {
-    width:100%;
-}
-
-.input-group .ng-invalid {
-    border: 2px solid #e74c3c;
-    border-radius: 3px;
-}
-
-/* Error Box */
-
-.alert{
-  padding: 15px;
-  margin-bottom: 20px;
-  border: 1px solid transparent;
-  border-radius: 2px;
-  border-color: #e85a71;
-  background-color:  #e85a71;
-  color: white;
-  
-}
-
-.help-block {
-    font-size: 12px;
-    color: white
-}</style>
-<body ng-controller="RegisterCtrl" ng-app="myApp">
- <div class="container">
-   <div id="signup">
-      <div class="signup-screen">
-         <div class="space-bot text-center">
-            <h1>Sign up</h1>
-           <div class="divider"></div>
-         </div>
-           <form class="form-register" method="post" name="register" novalidate action="index.php">
-	            <div class="input-field col s6">
-              <input id="first-name" type="text" class="validate" required>
-              <label for="first-name">First Name</label>
-            </div>
-            <div class="input-field col s6">
-              <input id="last-name" type="text" class="validate" required>
-              <label for="last-name">Last Name</label>
-             </div>
-             <div class="input-field col s6">
-              <input id="email" type="email" name="email" ng-model="email" class="validate" required>
-              <label for="email">Email</label>
-             </div>
-             <p class="alert alert-danger" ng-show="form-register.email.$error.email">Your email is invalid.</p>
-             <div class="input-field col s6">
-               <input id="password" type="password" name="password" ng-model="password" ng-minlength='6' class="validate" required>
-               <label for="password">Password</label>
-              </div>
-              <p class="alert alert-danger" ng-show="form-register.password.$error.minlength || form.password.$invalid">Your password must be at least 6 characters.</p>
-              <div class="space-top text-center">
-               <button ng-disabled="form-register.$invalid" class="waves-effect waves-light btn done">
-               <i class="material-icons left">done</i> Done
-               </button>
-               <button type="button" class="waves-effect waves-light btn cancel">
-               <i class="material-icons left">clear</i>Cancel
-               </button>
-              </div>
-             </form>
-           </div>
-        </div>
-    </div>
-    <script>
-    var myApp = angular.module("myApp", []);
-myApp.controller("RegisterCtrl", function ($scope) {
-
-});
-    </script>
+	<div class="box">
+		<form class="login-form" action="main.php">
+			<h1>Login</h1>
+			<input type="email" name="email" placeholder="email" required>
+			<input type="text" name="username" placeholder="Username" required>
+			<input type="password" name="password" placeholder="Password" required>
+			<input type="submit" name="Login" value="Login">
+			<div class="links">
+				<a href="#">Forgot Password</a>
+				<a href="/register.php">Sign Up</a>
+			</div>
+			</button>
+		</form>
+		
+	</div>
 </body>
 </html>
